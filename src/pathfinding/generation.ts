9d86b415a471c3e4c4c3ec1f6c6d79212f355c73
enix403 <fog.code000@gmail.com>
2024-09-15 00:40:09 +0500

    changes

--- src/pathfinding/generation.ts
+++ src/pathfinding/generation.ts
@@ -15,16 +15,23 @@ class Area {
     public readonly width: number,
     public readonly height: number,
     public readonly lastCutOrient: "h" | "v",
-    public readonly lastCutAt: number
+    public readonly lastDoorAt: number
   ) {}
 }
 
 function divideV(grid: Grid, area: Area, stack: Area[]) {
-  if (area.width <= 2) {
+  if (area.width <= 3) {
     return;
   }
 
   let cutAt = area.startX + 1 + Math.floor((area.width - 2) * Math.random());
+
+  if (area.lastCutOrient === 'h') {
+    if (area.lastDoorAt === cutAt) {
+      return;
+    }
+  }
+
   let doorAt = Math.floor(area.height * Math.random());
 
   for (let y = 0; y < area.height; ++y) {
@@ -33,13 +40,15 @@ function divideV(grid: Grid, area: Area, stack: Area[]) {
     else node.walkable = false;
   }
 
+  doorAt += area.startY;
+
   let area1 = new Area(
     area.startX,
     area.startY,
     cutAt - area.startX,
     area.height,
     "v",
-    cutAt
+    doorAt
   );
 
   let area2 = new Area(
@@ -48,7 +57,7 @@ function divideV(grid: Grid, area: Area, stack: Area[]) {
     area.startX + area.width - cutAt - 1,
     area.height,
     "v",
-    cutAt
+    doorAt
   );
 
   stack.push(area1);
@@ -56,12 +65,18 @@ function divideV(grid: Grid, area: Area, stack: Area[]) {
 }
 
 function divideH(grid: Grid, area: Area, stack: Area[]) {
-  if (area.height <= 2) {
+  if (area.height <= 3) {
     return;
   }
 
   let cutAt = area.startY + 1 + Math.floor((area.height - 2) * Math.random());
   let doorAt = Math.floor(area.width * Math.random());
+
+  if (area.lastCutOrient === 'v') {
+    if (area.lastDoorAt === cutAt) {
+      return;
+    }
+  }
 
   for (let x = 0; x < area.width; ++x) {
     let node = grid.getNode(area.startX + x, cutAt);
@@ -69,22 +84,29 @@ function divideH(grid: Grid, area: Area, stack: Area[]) {
     else node.walkable = false;
   }
 
+  doorAt += area.startX;
+
   let area1 = new Area(
-    area.startX, area.startY,
-    area.width, cutAt - area.startY,
-    "v", cutAt
+    area.startX,
+    area.startY,
+    area.width,
+    cutAt - area.startY,
+    "h",
+    doorAt
   );
 
   let area2 = new Area(
-    area.startX, cutAt + 1,
-    area.width, area.startY + area.height - cutAt - 1,
-    "v", cutAt
+    area.startX,
+    cutAt + 1,
+    area.width,
+    area.startY + area.height - cutAt - 1,
+    "h",
+    doorAt
   );
 
   stack.push(area1);
   stack.push(area2);
 }
-
 
 export function fillMaze(grid: Grid) {
   let root = new Area(0, 0, grid.NumTilesX, grid.NumTilesY, "v", -1);
@@ -94,9 +116,7 @@ export function fillMaze(grid: Grid) {
   while (stack.length > 0) {
     let area = stack.pop()!;
 
-    if (area.width > area.height)
-      divideV(grid, area, stack);
-    else
-      divideH(grid, area, stack);
+    if (area.width > area.height) divideV(grid, area, stack);
+    else divideH(grid, area, stack);
   }
 }
