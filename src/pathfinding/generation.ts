2edfe5969d3b5947c091a3c48aeb1cca05136f41
enix403 <fog.code000@gmail.com>
2024-09-15 00:28:39 +0500

    Tweaks

--- src/pathfinding/generation.ts
+++ src/pathfinding/generation.ts
@@ -20,14 +20,14 @@ class Area {
 }
 
 function divideV(grid: Grid, area: Area, stack: Area[]) {
-  if (area.width <= 4) {
+  if (area.width <= 2) {
     return;
   }
 
   let cutAt = area.startX + 1 + Math.floor((area.width - 2) * Math.random());
   let doorAt = Math.floor(area.height * Math.random());
 
-  for (let y = 1; y < area.height - 1; ++y) {
+  for (let y = 0; y < area.height; ++y) {
     let node = grid.getNode(cutAt, area.startY + y);
     if (y === doorAt) node.walkable = true;
     else node.walkable = false;
@@ -55,6 +55,37 @@ function divideV(grid: Grid, area: Area, stack: Area[]) {
   stack.push(area2);
 }
 
+function divideH(grid: Grid, area: Area, stack: Area[]) {
+  if (area.height <= 2) {
+    return;
+  }
+
+  let cutAt = area.startY + 1 + Math.floor((area.height - 2) * Math.random());
+  let doorAt = Math.floor(area.width * Math.random());
+
+  for (let x = 0; x < area.width; ++x) {
+    let node = grid.getNode(area.startX + x, cutAt);
+    if (x === doorAt) node.walkable = true;
+    else node.walkable = false;
+  }
+
+  let area1 = new Area(
+    area.startX, area.startY,
+    area.width, cutAt - area.startY,
+    "v", cutAt
+  );
+
+  let area2 = new Area(
+    area.startX, cutAt + 1,
+    area.width, area.startY + area.height - cutAt - 1,
+    "v", cutAt
+  );
+
+  stack.push(area1);
+  stack.push(area2);
+}
+
+
 export function fillMaze(grid: Grid) {
   let root = new Area(0, 0, grid.NumTilesX, grid.NumTilesY, "v", -1);
 
@@ -63,6 +94,9 @@ export function fillMaze(grid: Grid) {
   while (stack.length > 0) {
     let area = stack.pop()!;
 
-    divideV(grid, area, stack);
+    if (area.width > area.height)
+      divideV(grid, area, stack);
+    else
+      divideH(grid, area, stack);
   }
 }
