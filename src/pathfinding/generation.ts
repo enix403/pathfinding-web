63ae74e35a015ca3ddf5afc6627e46ccadb3e19d
enix403 <fog.code000@gmail.com>
2024-09-15 21:45:10 +0500

    Generate maze

--- src/pathfinding/generation.ts
+++ src/pathfinding/generation.ts
@@ -1,122 +1,85 @@
 import { Grid } from "./Grid";
 
-function clamp(val: number, min: number, max: number) {
-  if (val < min) return min;
-
-  if (val > max) return max;
-
-  return val;
+function randrange(min: number, max: number) {
+  return min + Math.floor(Math.random() * (max - min + 1));
 }
 
-class Area {
-  constructor(
-    public readonly startX: number,
-    public readonly startY: number,
-    public readonly width: number,
-    public readonly height: number,
-    public readonly lastCutOrient: "h" | "v",
-    public readonly lastDoorAt: number
-  ) {}
-}
+function subdivide(
+  grid: Grid,
+  startX: number,
+  endX: number,
+  startY: number,
+  endY: number,
+  isHorizontal: boolean // If this next cut should be horizontal or not.
+) {
+  let width = endX - startX + 1;
+  let height = endY - startY + 1;
 
-function divideV(grid: Grid, area: Area, stack: Area[]) {
-  if (area.width <= 3) {
-    return;
-  }
-
-  let cutAt = area.startX + 1 + Math.floor((area.width - 2) * Math.random());
-
-  if (area.lastCutOrient === 'h') {
-    if (area.lastDoorAt === cutAt) {
+  if (isHorizontal) {
+    if (height <= 2) {
       return;
     }
-  }
 
-  let doorAt = Math.floor(area.height * Math.random());
+    let wallY = Math.floor(randrange(startY, endY) / 2) * 2;
+    let holeX = Math.floor(randrange(startX, endX) / 2) * 2 + 1;
 
-  for (let y = 0; y < area.height; ++y) {
-    let node = grid.getNode(cutAt, area.startY + y);
-    if (y === doorAt) node.walkable = true;
-    else node.walkable = false;
-  }
+    for (var x = startX; x <= endX; ++x) {
+      if (x !== holeX) {
+        let node = grid.getNode(x, wallY);
+        node.walkable = false;
+      }
+    }
 
-  doorAt += area.startY;
-
-  let area1 = new Area(
-    area.startX,
-    area.startY,
-    cutAt - area.startX,
-    area.height,
-    "v",
-    doorAt
-  );
-
-  let area2 = new Area(
-    cutAt + 1,
-    area.startY,
-    area.startX + area.width - cutAt - 1,
-    area.height,
-    "v",
-    doorAt
-  );
-
-  stack.push(area1);
-  stack.push(area2);
-}
-
-function divideH(grid: Grid, area: Area, stack: Area[]) {
-  if (area.height <= 3) {
-    return;
-  }
-
-  let cutAt = area.startY + 1 + Math.floor((area.height - 2) * Math.random());
-  let doorAt = Math.floor(area.width * Math.random());
-
-  if (area.lastCutOrient === 'v') {
-    if (area.lastDoorAt === cutAt) {
+    subdivide(grid, startX, endX, startY, wallY - 1, false);
+    subdivide(grid, startX, endX, wallY + 1, endY, false);
+  } else {
+    // Vertical cut
+    if (width <= 2) {
       return;
     }
+
+    let wallX = Math.floor(randrange(startX, endX) / 2) * 2;
+    let holeY = Math.floor(randrange(startY, endY) / 2) * 2 + 1;
+
+    for (var y = startY; y <= endY; ++y) {
+      if (y !== holeY) {
+        let node = grid.getNode(wallX, y);
+        node.walkable = false;
+      }
+    }
+
+    subdivide(grid, startX, wallX - 1, startY, endY, true);
+    subdivide(grid, wallX + 1, endX, startY, endY, true);
+  }
+}
+
+function wrapOuterWalls(grid: Grid, width: number, height: number) {
+  for (let x = 0; x < width; ++x) {
+    let nodeTop = grid.getNode(x, 0);
+    let nodeBottom = grid.getNode(x, height - 1);
+
+    nodeTop.walkable = false;
+    nodeBottom.walkable = false;
   }
 
-  for (let x = 0; x < area.width; ++x) {
-    let node = grid.getNode(area.startX + x, cutAt);
-    if (x === doorAt) node.walkable = true;
-    else node.walkable = false;
+  for (let y = 0; y < height; ++y) {
+    let nodeLeft = grid.getNode(0, y);
+    let nodeRight = grid.getNode(width - 1, y);
+
+    nodeLeft.walkable = false;
+    nodeRight.walkable = false;
   }
-
-  doorAt += area.startX;
-
-  let area1 = new Area(
-    area.startX,
-    area.startY,
-    area.width,
-    cutAt - area.startY,
-    "h",
-    doorAt
-  );
-
-  let area2 = new Area(
-    area.startX,
-    cutAt + 1,
-    area.width,
-    area.startY + area.height - cutAt - 1,
-    "h",
-    doorAt
-  );
-
-  stack.push(area1);
-  stack.push(area2);
 }
 
 export function fillMaze(grid: Grid) {
-  let root = new Area(0, 0, grid.NumTilesX, grid.NumTilesY, "v", -1);
 
-  let stack = [root];
+  let width = grid.NumTilesX;
+  let height = grid.NumTilesY;
 
-  while (stack.length > 0) {
-    let area = stack.pop()!;
+  if (width % 2) width--;
 
-    if (area.width > area.height) divideV(grid, area, stack);
-    else divideH(grid, area, stack);
-  }
+  if (height % 2) height--;
+
+  wrapOuterWalls(grid, width, height);
+  subdivide(grid, 1, width - 2, 1, height - 2, false);
 }
