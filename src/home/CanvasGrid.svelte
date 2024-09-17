ab1f9852fc3fe3a29a5735e9559888c234ff8f08
enix403 <fog.code000@gmail.com>
2024-09-17 12:56:26 +0500

    Add nodes

---
+++ src/home/CanvasGrid.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { PathFindingScene2 } from "./PathFindingScene2";
+
+  let canvas: HTMLCanvasElement | null = null;
+
+  onMount(() => {
+    return PathFindingScene2.createGame(canvas!);
+  });
+</script>
+
+<canvas bind:this={canvas} class="w-full h-full" />
