c9d6caaf43f40a632b481cef87cac6e48f446e48
enix403 <fog.code000@gmail.com>
2024-09-17 12:25:51 +0500

    Maze

--- src/home/Controls.svelte
+++ src/home/Controls.svelte
@@ -1,31 +1,42 @@
 <script>
   import clsx from "clsx";
-  import AlgorithmOption from "./AlgorithmOption.svelte";
+  import DropdownOption from "./DropdownOption.svelte";
+
   import { IconCpu } from "@tabler/icons-svelte";
 
   const algorithms = [
     { title: "Breadth First Search" },
     { title: "Depth First Search" },
     { title: "A-star Algorithm" },
-    { title: "Djikstra Algorithm" },
+    { title: "Djikstra Algorithm" }
   ];
 
-  let selectedIndex = -1;
+  let selectedAlgorithmIndex = -1;
+
+  const mazeFills = [
+    { title: "No Maze" },
+    { title: "Recurisve Divide" },
+    { title: "Recurisve Backtracking" },
+    { title: "Binary Tree Maze" }
+  ];
+
+  let selectedMazeIndex = 0;
 </script>
 
-<div class="dropdown w-full">
+<p class="font-semibold text-sm">Select Algorithm</p>
+<div class="dropdown w-full mt-2">
   <button
     class={clsx(
       "btn btn-block gap-x-1",
-      selectedIndex === -1 ? "btn-solid-primary" : "btn-solid-success"
+      selectedAlgorithmIndex === -1 ? "btn-solid-primary" : "btn-solid-success"
     )}
   >
-    {#if selectedIndex === -1}
+    {#if selectedAlgorithmIndex === -1}
       <IconCpu />
       Select Algorithm
     {:else}
       <strong>
-        {algorithms[selectedIndex].title}
+        {algorithms[selectedAlgorithmIndex].title}
       </strong>
     {/if}
   </button>
@@ -34,11 +45,36 @@
     class="dropdown-menu dropdown-menu-bottom-right border border-gray-300 mt-2"
   >
     {#each algorithms as algorithm, index (index)}
-      <AlgorithmOption
-        active={index === selectedIndex}
+      <DropdownOption
+        active={index === selectedAlgorithmIndex}
         title={algorithm.title}
         on:click={() => {
-          selectedIndex = index;
+          selectedAlgorithmIndex = index;
+          // @ts-ignore
+          document.activeElement?.blur();
+        }}
+      />
+    {/each}
+  </div>
+</div>
+
+<p class="font-semibold text-sm mt-4">Select Maze</p>
+<div class="dropdown w-full mt-2">
+  <button class={clsx("btn btn-block gap-x-1 btn-solid-success")}>
+    <strong>
+      {mazeFills[selectedMazeIndex].title}
+    </strong>
+  </button>
+
+  <div
+    class="dropdown-menu dropdown-menu-bottom-right border border-gray-300 mt-2"
+  >
+    {#each mazeFills as mazeFill, index (index)}
+      <DropdownOption
+        active={index === selectedMazeIndex}
+        title={mazeFill.title}
+        on:click={() => {
+          selectedMazeIndex = index;
           // @ts-ignore
           document.activeElement?.blur();
         }}
